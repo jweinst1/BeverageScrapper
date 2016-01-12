11a5fc7f202695af57bd6833b8b6f477951e9132
Joshua Weinstein <jweinst1@berkeley.edu>
2016-01-11 22:50:46 -0800

    more funds

--- lib/BeverageScrapper.rb
+++ lib/BeverageScrapper.rb
@@ -8,7 +8,47 @@ module BeverageScrapper
    end
    #list of strings of softdrink brands
     def self.brands
-      %w(coke sprite pepsi dr.pepper drpepper cocacola cokezero mountaindew minutemaid)
+      %w(coke sprite pepsi dr.pepper drpepper cocacola cokezero mountaindew minutemaid fanta pepsi-cola a&w 7up mug cola dasani)
+    end
+    def self.positives
+      %w(refreshing cold tasty wonderful delicious yummy flavorful great fresh)
+    end
+    def self.neutrals
+      %w(bubbly cold icy flavored strong)
+    end
+    def self.flavor
+      %w(bitter sweet sour tangy minty savory lemony acidic bittersweet creamy citrusy dry fizzy full-bodied nutty bland salty spicy tangy tart zesty zingy)
+    end
+    def self.negatives
+      %w(yucky disgusting horrid gross awful flavorless bland plain boring bad raunchy nasty tasteless
+ stale flat)
+    end
+  end
+  class Utils
+    
+    #counts all the words in a text in linear time
+    def self.wordcount(text)
+      words = text.split(' ')
+      wc = Hash.new
+      for elem in words
+        if wc.has_key?(elem)
+          wc[elem] += 1
+        else
+          wc[elem] = 1
+        end
+      end
+      return wc
+    end
+
+    #checks if any word in an array is in the text
+    def self.hasany(words, text)
+      words = text.split(' ')
+      for elem in words
+        if words.include?(elem)
+          return true
+        end
+      end
+      return false
     end
   end
 end
